84db4ef097725e04a24327391dd96665ac3f09ae
abhishekkumarpatel123 <abhishekpatel.aith@gmail.com>
2019-09-01 09:47:32 +0530

    Simple Interest Calculator Completed

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+        SimpleInterestCalculatorModel Model = new SimpleInterestCalculatorModel();
+        public MainPage()
+		{
+            BindingContext = this.Model;
+            InitializeComponent();
+            
+		}
+
+        private void Calculate_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(this.Model.Prinipal_Amount) || string.IsNullOrEmpty(this.Model.Rate) || string.IsNullOrEmpty(this.Model.Time))
+                {
+                    DisplayAlert("Alert!", "Please Enter Mandatory Items", "OK");
+                    return;
+                }
+                else
+                    this.Model.CalculatedValue = (Convert.ToDouble(this.Model.Prinipal_Amount) * Convert.ToDouble(this.Model.Rate) * Convert.ToDouble(this.Model.Time) / 100).ToString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void refresh_Clicked(object sender, EventArgs e)
+        {
+            this.Model.Prinipal_Amount = null;
+            this.Model.Time = null;
+            this.Model.CalculatedValue = null;
+            this.Model.Rate = null;
+        }
+    }
+}
---
+++ App1/App1/SimpleInterestCalculatorModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App1
+{
+    public class SimpleInterestCalculatorModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _Prinipal_Amount;
+        public string Prinipal_Amount
+        {
+            get
+            {
+                return this._Prinipal_Amount;
+            }
+            set { this._Prinipal_Amount = value;this.OnPropertyChanged("Prinipal_Amount"); }
+        }
+        private string _Rate;
+        public string Rate
+        {
+            get { return this._Rate; }
+            set { this._Rate = value;this.OnPropertyChanged("Rate"); }
+        }
+        private string _Time;
+        public string Time
+        {
+            get { return this._Time; }
+            set { this._Time = value;this.OnPropertyChanged("Time"); }
+        }
+        private string _CalculatedValue;
+        public string CalculatedValue
+        {
+            get { return this._CalculatedValue; }
+            set { this._CalculatedValue = value;this.OnPropertyChanged("CalculatedValue"); }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
